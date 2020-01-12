da1e3db34d60a68dfe4ebc98b86327295e939aa7
FatmaKERFAHI <fatma.kerfahi@gmail.com>
2020-01-12 22:05:35 +0100

    remove classes

--- src/main/java/com/softeam/flight/controllers/CountryController.java
+++
@@ -1,5 +0,0 @@
-package com.softeam.flight.controllers;
-
-public class CountryController {
-
-}
--- src/main/java/com/softeam/flight/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.softeam.flight.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.softeam.flight.entities.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
